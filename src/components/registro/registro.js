89c14442fb07f5c4020922238335d71af1dc3c99
Andrés Liu <adavidliuf@gmail.com>
2019-07-08 21:48:53 -0500

    Avances en el formulario de registro individual

--- src/components/registro/registro.js
+++ src/components/registro/registro.js
@@ -4,16 +4,19 @@ import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormLabel from '@material-ui/core/FormLabel';
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import RadioGroup from '@material-ui/core/RadioGroup';
 import Radio from "@material-ui/core/Radio";
+import FormGroup from '@material-ui/core/FormGroup';
 import FormControl from "@material-ui/core/FormControl";
 import MenuItem from "@material-ui/core/MenuItem";
 import InputLabel from "@material-ui/core/InputLabel";
 import OutlinedInput from "@material-ui/core/OutlinedInput";
 import Select from "@material-ui/core/Select";
+import Checkbox from '@material-ui/core/Checkbox';
 
 import Establecimiento from "../establecimiento/establecimiento";
 
@@ -21,13 +24,23 @@ class Registro extends Component {
     constructor() {
         super();
         this.state = {
-            tipoPerfil: "Institucional",
+            tipoPerfil: "Individual",
             pais: "",
             departamento: "",
             municipio: "",
             nombreIE: "",
             numSedes: "",
             sedes: [],
+            nombres: "",
+            apellidos: "",
+            fechaNacimiento: "",
+            educacionMaxima: "",
+            descriptionPersonal: "",
+            aniosExperiencia: 0,
+            representaInstitucionEducativa: "",
+            cualInstitucionRepresenta: "",
+            esDirectivo: "",
+            tipoDirectivo: "",
             isCompletado: false
         }
     }
@@ -51,7 +64,7 @@ class Registro extends Component {
         this.state.sedes.push(nuevaInfoSede);
     }
 
-    /* Simulando cuando el registro se haya completado, para traer los datos de cada sede al estado */
+    /* Simulando cuando el registro se haya completado, para traer los datos de cada sede al estado. Debe quedar dentro del método "registrar" */
     handleClick = () => {
         this.setState({
             ...this.state,
@@ -168,7 +181,202 @@ class Registro extends Component {
                 </React.Fragment>
 			);
         } else {
-            formulario = "";
+            formulario = (
+                <React.Fragment>
+                    <Typography variant="body1" className="mb-2"><strong>Datos personales</strong></Typography>
+                    <form onSubmit={this.registrar}>
+                        <Grid container spacing={2} className="mb-2">
+                            <Grid item xs={12} md={6} className="py-0">
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="nombres"
+                                    label="Nombres"
+                                    name="nombres"
+                                    onChange={this.handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12} md={6} className="py-0">
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="apellidos"
+                                    label="Apellidos"
+                                    name="apellidos"
+                                    onChange={this.handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12} className="py-0">
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="fechaNacimiento"
+                                    label="Fecha de Nacimiento"
+                                    name="fechaNacimiento"
+                                    type="date"
+                                    defaultValue="1950-05-24"
+                                    onChange={this.handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12} className="py-0">
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    multiline
+                                    inputProps={{ maxLength: 400 }}
+                                    rows="5"
+                                    id="descriptionPersonal"
+                                    label="Descripción personal"
+                                    name="descriptionPersonal"
+                                    type="text"
+                                    onChange={this.handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12} className="py-0">
+                                <FormControl variant="outlined" className="w-100 mt-4 mt-md-3 mb-2">
+                                    <InputLabel htmlFor="educacionMaxima">Educación formal máxima (alcanzada o en curso) *</InputLabel>
+                                    <Select
+                                        value={this.state.educacionMaxima}
+                                        onChange={this.handleChange}
+                                        input={<OutlinedInput required name="educacionMaxima" id="educacionMaxima"/>}
+                                    >
+                                        <MenuItem value="Bachiller">Bachiller</MenuItem>
+                                        <MenuItem value="Técnico">Técnico</MenuItem>
+                                        <MenuItem value="Tecnólogo">Tecnólogo</MenuItem>
+                                        <MenuItem value="Profesional">Profesional</MenuItem>
+                                        <MenuItem value="Especialista">Especialista</MenuItem>
+                                        <MenuItem value="Magister">Magister</MenuItem>
+                                        <MenuItem value="Doctorado">Doctorado</MenuItem>
+                                        <MenuItem value="Postdoctorado">Postdoctorado</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={12} className="py-0">
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="aniosExperiencia"
+                                    label="¿Cuántos años tiene de experiencia docente?"
+                                    name="aniosExperiencia"
+                                    type="number"
+                                    onChange={this.handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12} className="py-0">
+                                <FormControl variant="outlined" className="w-100 mt-4 mt-md-3 mb-2">
+                                    <InputLabel htmlFor="representaInstitucionEducativa">¿Representa a una institución educativa? *</InputLabel>
+                                    <Select
+                                        value={this.state.representaInstitucionEducativa}
+                                        onChange={this.handleChange}
+                                        input={<OutlinedInput required name="representaInstitucionEducativa" id="representaInstitucionEducativa"/>}
+                                    >
+                                        <MenuItem value={true}>Sí</MenuItem>
+                                        <MenuItem value={false}>No</MenuItem>
+                                    </Select>
+                                </FormControl>
+                                {this.state.representaInstitucionEducativa ? (
+                                    <FormControl variant="outlined" className="w-100 mt-4 mt-md-3 mb-2">
+                                        <InputLabel htmlFor="cualInstitucionRepresenta">¿Cuál? *</InputLabel>
+                                        <Select
+                                            value={this.state.cualInstitucionRepresenta}
+                                            onChange={this.handleChange}
+                                            input={<OutlinedInput required name="cualInstitucionRepresenta" id="cualInstitucionRepresenta"/>}
+                                        >
+                                            <MenuItem value="Item 1">Item 1</MenuItem>
+                                            <MenuItem value="Otra">Otra</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                ) : ""}
+                                {this.state.cualInstitucionRepresenta === "Otra" ? (
+                                    <Typography variant="body2">Por favor consulte con su Insitución Educativa para que primero se realice el registro de ésta, o realícelo usted mismo.</Typography>
+                                ) : ""}
+                            </Grid>
+                            {this.state.cualInstitucionRepresenta !== "Otra" ? (
+                                <React.Fragment>
+                                    <Grid item xs={12} className="py-0">
+                                        <FormControl variant="outlined" className="w-100 mt-4 mt-md-3 mb-2">
+                                            <InputLabel htmlFor="esDirectivo">¿Es directivo docente? *</InputLabel>
+                                            <Select
+                                                value={this.state.esDirectivo}
+                                                onChange={this.handleChange}
+                                                input={<OutlinedInput required name="esDirectivo" id="esDirectivo"/>}
+                                            >
+                                                <MenuItem value={true}>Sí</MenuItem>
+                                                <MenuItem value={false}>No</MenuItem>
+                                            </Select>
+                                        </FormControl>
+                                        {this.state.esDirectivo ? (
+                                            <FormControl variant="outlined" className="w-100 mt-4 mt-md-3 mb-2">
+                                                <InputLabel htmlFor="tipoDirectivo">Tipo de directivo docente *</InputLabel>
+                                                <Select
+                                                    value={this.state.tipoDirectivo}
+                                                    onChange={this.handleChange}
+                                                    input={<OutlinedInput required name="tipoDirectivo" id="tipoDirectivo"/>}
+                                                >
+                                                    <MenuItem value="Rector">Rector</MenuItem>
+                                                    <MenuItem value="Director">Director</MenuItem>
+                                                    <MenuItem value="Coordinador">Coordinador</MenuItem>
+                                                </Select>
+                                            </FormControl>
+                                        ) : ""}
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <FormLabel component="legend">¿En qué niveles enseña?</FormLabel>
+                                        <FormGroup>
+                                            <FormControlLabel
+                                                control={<Checkbox onChange={this.handleChange} color="primary" value="No aplica" />}
+                                                label="No aplica"
+                                            />
+                                            <FormControlLabel
+                                                control={<Checkbox onChange={this.handleChange} color="primary" value="Preescolar" />}
+                                                label="Preescolar"
+                                            />
+                                            <FormControlLabel
+                                                control={<Checkbox onChange={this.handleChange} color="primary" value="Básica primaria" />}
+                                                label="Básica primaria"
+                                            />
+                                            <FormControlLabel
+                                                control={<Checkbox onChange={this.handleChange} color="primary" value="Básica secundaria" />}
+                                                label="Básica secundaria"
+                                            />
+                                            <FormControlLabel
+                                                control={<Checkbox onChange={this.handleChange} color="primary" value="Media académica" />}
+                                                label="Media académica"
+                                            />
+                                            <FormControlLabel
+                                                control={<Checkbox onChange={this.handleChange} color="primary" value="Media técnica" />}
+                                                label="Media técnica"
+                                            />
+                                        </FormGroup>
+                                    </Grid>
+                                </React.Fragment>
+                            ) : ""}
+                        </Grid>
+                        {this.state.cualInstitucionRepresenta !== "Otra" ? (
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className="mt-2"
+                                size="large"
+                                onClick={this.handleClick}
+                            >
+                                Registrarme
+                            </Button>
+                        ) : ""}
+                    </form>
+                </React.Fragment>
+            );
         }
 
 		return (
@@ -176,14 +384,14 @@ class Registro extends Component {
 				<CssBaseline />
 				<div className="py-5">
                     <div className="text-center mb-2">
-					    <Typography component="h1" variant="h5" className="mb-4"><strong>Formulario de registro</strong></Typography>
+					    <Typography component="h1" variant="h5" className="mb-4"><strong>Registro</strong></Typography>
                         <FormControl component="fieldset" className="w-100">
                             <Grid container>
                                 <Grid item xs={12}>
                                     <hr/>
                                 </Grid>
                                 <Grid item xs={12}>
-                                    <Typography variant="body1" className="mb-2"><strong>Seleccione su tipo de perfil</strong></Typography>
+                                    <Typography variant="body1" className="mb-2"><strong>¿Qué tipo de perfil desea registrar?</strong></Typography>
                                 </Grid>
                                 <Grid item xs={12}>
                                     <RadioGroup
