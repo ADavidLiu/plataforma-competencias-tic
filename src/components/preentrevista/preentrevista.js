8551c2433e0682663990fb773ead15168d5b4d23
Andrés Liu <adavidliuf@gmail.com>
2019-07-26 16:58:25 -0500

    Avances en la implementación del nuevo esquema del JSON para la pre-entrevista

--- src/components/preentrevista/preentrevista.js
+++ src/components/preentrevista/preentrevista.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import { Redirect } from "react-router-dom";
 
-import preguntas from "../../models/preentrevista";
+import preguntas from "../../models/preentrevista-new";
 
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
@@ -21,10 +21,13 @@ class Preentrevista extends Component {
         this.state = {
             tipoUsuario: "",
             respuestas: [],
-            preguntasVisibles: []
+            preguntas: preguntas,
+            preguntasVisibles: [],
+            visibilityClasses: []
         }
 
         this.parentLabel = "";
+        this.subniveles = [];
         this.arrayPreguntas = [];
     }
 
@@ -52,11 +55,317 @@ class Preentrevista extends Component {
             tipoUsuario: infoCargada.tipoUsuario
         });
         
-        this.prepararPreguntas();
+        /* this.prepararPreguntas(); */
+        /* this.crearPreguntas(); */
+        this.crearCuestionario();
     }
 
-    handlePreguntaChange = (e, rootIndex) => {
+    handlePreguntaChange = (e, preguntaID, typeOfLevel, triggerID, index) => {
+        /* console.log(preguntaID, e.target.value); */
+        const selectedOptionID = `${preguntaID}.${e.target.value}`;
+        /* console.log("Buscando... ", selectedOptionID); */
+        const newRespuestas = [...this.state.respuestas];
 
+        newRespuestas[index] = e.target.value;
+
+        const newPreguntasVisibles = [...this.state.preguntasVisibles];
+        const newVisibilityClasses = [...this.state.visibilityClasses];
+        const encontrados = [];
+
+        /* Primero, reiniciar ocultando todo excepto los niveles root */
+        newPreguntasVisibles.forEach(pregunta => {
+            if (pregunta.props.typeoflevel !== "ROOT") {
+                newVisibilityClasses[pregunta.props.index] = "mb-4 d-none";
+            } else {
+                newVisibilityClasses[pregunta.props.index] = "mb-4";
+            }
+        });
+
+        /* Después, mostrar la selección correcta */
+        newPreguntasVisibles.forEach(pregunta => {
+            if (pregunta.props.triggerid === selectedOptionID) {
+                encontrados.push(pregunta.props);
+            }
+        });
+
+        encontrados.forEach(pregunta => {
+            newVisibilityClasses[pregunta.index] = "mb-4";
+        });
+
+        this.setState({
+            respuestas: newRespuestas,
+            preguntasVisibles: newPreguntasVisibles,
+            visibilityClasses: newVisibilityClasses
+        });
+
+        /* Después del cambio. Ya hay que seguir carrying el delay del inicio */
+        const timeout = setTimeout(() => {
+            console.log(this.state.preguntasVisibles);
+            console.log(this.state.visibilityClasses);
+            clearTimeout(timeout);
+        }, 100);
+    }
+
+    crearCuestionario = () => {
+        const newPreguntasVisibles = [...this.state.preguntasVisibles];
+        const newVisibilityClasses = [...this.state.visibilityClasses];
+
+        this.state.preguntas.map((pregunta, i) => {
+            let opcionesRespuesta = "";
+            let triggeredBy;
+
+            if (pregunta.isTriggeredBy) {
+                triggeredBy = pregunta.isTriggeredBy;
+            }
+
+            switch (pregunta.typeOfAnswer) {
+                case "RADIO":
+                    opcionesRespuesta = <RadioGroup
+                        name={pregunta.label}
+                        value={this.state.respuestas[i]}
+                        onChange={e => this.handlePreguntaChange(e, pregunta.id, pregunta.typeOfLevel, pregunta.isTriggerFor, i)}
+                    >
+                        <FormControlLabel
+                            key="Sí"
+                            value="0"
+                            control={<Radio required name={pregunta.label} color="primary" />}
+                            label="Sí"
+                        />
+                        <FormControlLabel
+                            key="No"
+                            value="1"
+                            control={<Radio required name={pregunta.label} color="primary" />}
+                            label="No"
+                        />
+                    </RadioGroup>
+                    break;
+                case "CHECKBOX":
+                    const opciones = [];
+                    pregunta.options.forEach((option, j) => {
+                        opciones.push(
+                            <FormControlLabel
+                                key={option}
+                                istriggeredby={triggeredBy}
+                                control={<Checkbox onChange={e => this.handlePreguntaChange(e, pregunta.id, pregunta.typeOfLevel, triggeredBy)} color="primary" 
+                                value={j} name={option} />}
+                                label={option}
+                            />
+                        );
+                    });
+                    opcionesRespuesta = opciones;
+                    break;
+                case "INPUT":
+                    opcionesRespuesta = (
+                        <TextField
+                            key={pregunta.label}
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            multiline
+                            inputProps={{ maxLength: 400 }}
+                            rows="5"
+                            label="Escriba su respuesta..."
+                            name={pregunta.label}
+                            onChange={e => this.handlePreguntaChange(e, pregunta.id, pregunta.typeOfLevel, triggeredBy)}
+                        />
+                    );
+                    break;
+            }
+
+            newVisibilityClasses[i] = "mb-4";
+
+            if (pregunta.typeOfLevel !== "ROOT") {
+                newVisibilityClasses[i] = "mb-4 d-none";
+            }
+
+            this.setState({
+                visibilityClasses: newVisibilityClasses
+            });
+
+            /* Pequeño delay para alcanzar a que las clases de visibilidad de inicialicen */
+            const timeout = setTimeout(() => {
+                const newPregunta = (
+                    <div key={i} index={pregunta.id} typeoflevel={pregunta.typeOfLevel} triggerid={!triggeredBy ? pregunta.isTriggerFor : triggeredBy} className={this.state.visibilityClasses[i]}>
+                        <Typography variant="body1" className="mb-3"><strong>{pregunta.label}</strong></Typography>
+                        {opcionesRespuesta}
+                    </div>
+                );
+                newPreguntasVisibles.push(newPregunta);
+
+                this.setState({
+                    preguntasVisibles: newPreguntasVisibles
+                });
+
+                clearTimeout(timeout);
+            }, 100);
+        });
+    }
+
+    crearNiveles = (pregunta, arrayBase) => {
+        console.log("----- NUEVO NIVEL -----");
+        console.log(pregunta);
+        const preguntaArray = [...arrayBase];
+
+        /* console.log("****PREGUNTA_ARRAY****");
+        console.log(preguntaArray);
+        console.log("****PREGUNTA_ARRAY****"); */
+
+        if (Array.isArray(pregunta)) {
+            console.log("Es array");
+            pregunta.forEach(subpregunta => {
+                let opcionesInternas;
+                let subpreguntaArray = [];
+    
+                if (!subpregunta.typeOfAnswer) {
+                    console.log("Sólo imprimir label");
+                    opcionesInternas = (
+                        <Typography variant="body1">{subpregunta.label}</Typography>
+                    );
+                } else {
+                    console.log("Crear opciones de respuesta");
+                    
+                    switch (subpregunta.typeOfAnswer) {
+                        case "RADIO":
+                            console.log("Creó RADIO");
+                            opcionesInternas = (
+                                <div>
+                                    <Typography variant="body1">{subpregunta.label}</Typography>
+                                    <RadioGroup
+                                        name={subpregunta.label}
+                                        onChange={e => this.handlePreguntaChange(e)}
+                                    >
+                                        <FormControlLabel
+                                            key="Sí"
+                                            value="Sí"
+                                            control={<Radio required name={`${subpregunta.label}-radio`} color="primary" />}
+                                            label="Sí"
+                                        />
+                                        <FormControlLabel
+                                            key="No"
+                                            value="No"
+                                            control={<Radio required name={`${subpregunta.label}-radio`} color="primary" />}
+                                            label="No"
+                                        />
+                                    </RadioGroup>
+                                </div>
+                            );
+                            break;
+                        case "CHECKBOX":
+                            console.log("Creó CHECKBOX");
+                            opcionesInternas = (
+                                <div>
+                                    {/* <Typography variant="body1">{subpregunta.label}</Typography> */}
+                                    <FormControlLabel
+                                        key={`${subpregunta.label}-checkbox`}
+                                        className="mt-3"
+                                        control={<Checkbox onChange={e => this.handlePreguntaChange(e)} color="primary" 
+                                        value={subpregunta.label} name={subpregunta.label} />}
+                                        label={subpregunta.label}
+                                    />
+                                </div>
+                            );
+                            break;
+                        case "INPUT":
+                            console.log("Creó INPUT");
+                            opcionesInternas = (
+                                <div>
+                                    <Typography variant="body1">{subpregunta.label}</Typography>
+                                    <TextField
+                                        key={`${subpregunta.label}-input`}
+                                        variant="outlined"
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        multiline
+                                        inputProps={{ maxLength: 400 }}
+                                        rows="5"
+                                        label="Escriba su respuesta..."
+                                        name={subpregunta.label}
+                                        onChange={e => this.handlePreguntaChange(e)}
+                                    />
+                                </div>
+                            );
+                            break;
+                        default:
+                            console.log("Creó NADA");
+                            opcionesInternas = null;
+                            break;
+                    }
+
+                    subpreguntaArray.push(
+                        <div key={subpregunta.label}>
+                            {opcionesInternas}
+                        </div>
+                    );
+    
+                    /* preguntaArray.push(
+                        <div key={subpregunta.label}>
+                            {opcionesInternas}
+                        </div>
+                    ); */
+                }
+    
+                if (subpregunta.options) {
+                    console.log("Sigue subdividido");
+                    return this.crearNiveles(subpregunta, preguntaArray);
+                } else {
+                    console.log("Ya terminó");
+                    preguntaArray.push(subpreguntaArray);
+                    return preguntaArray;
+                }
+            });
+        } else {
+            console.log("No es array. Sigue subdividido. Leer opciones.");
+            return this.crearNiveles(pregunta.options, preguntaArray);
+        }
+
+        this.subniveles.push(preguntaArray);
+    }
+
+    crearPreguntas = () => {
+        const nuevasPreguntasVisibles = [...this.state.preguntasVisibles];
+
+        /* Esto crea sólo el nivel root de cada pregunta */
+        this.state.preguntas.map((pregunta, rootIndex) => {
+            console.log("---------- NUEVA PREGUNTA ----------");
+
+            if (pregunta.options) {
+                /* Esto crea a partir del tercer nivel, después del Sí/No inicial */
+                this.crearNiveles(pregunta.options[0].options, []);
+            }
+
+            const rootPregunta = (
+                <React.Fragment key={pregunta.label}>
+                    <Typography variant="body1" className="mb-2"><strong>{pregunta.label}</strong></Typography>
+                    <RadioGroup
+                        name={pregunta.label}
+                        onChange={e => this.handlePreguntaChange(e)}
+                    >
+                        <FormControlLabel
+                            key="Sí"
+                            value="Sí"
+                            control={<Radio required name={`${pregunta.label}-radio`} color="primary" />}
+                            label="Sí"
+                        />
+                        <FormControlLabel
+                            key="No"
+                            value="No"
+                            control={<Radio required name={`${pregunta.label}-radio`} color="primary" />}
+                            label="No"
+                        />
+                    </RadioGroup>
+                    {this.subniveles}
+                    <hr/>
+                </React.Fragment>
+            );
+
+            nuevasPreguntasVisibles.push(rootPregunta);
+        });
+
+        this.setState({
+            preguntasVisibles: nuevasPreguntasVisibles
+        });
     }
 
     prepararPreguntas = () => {
@@ -108,6 +417,7 @@ class Preentrevista extends Component {
             case "CHECKBOX":
                 enunciado = <Typography key={this.parentLabel}>{this.parentLabel}</Typography>;
                 opciones = <FormControlLabel
+                    key={`${pregunta.label}-checkbox`}
                     className="mt-3"
                     control={<Checkbox onChange={e => this.handlePreguntaChange(e)} color="primary" 
                     value={pregunta.label} name={pregunta.label} />}
@@ -117,6 +427,7 @@ class Preentrevista extends Component {
             case "INPUT":
                 enunciado = <Typography key={pregunta.label}>{pregunta.label}</Typography>;
                 opciones = <TextField
+                    key={`${pregunta.label}-input`}
                     variant="outlined"
                     margin="normal"
                     required
@@ -124,7 +435,7 @@ class Preentrevista extends Component {
                     multiline
                     inputProps={{ maxLength: 400 }}
                     rows="5"
-                    label="Digite su respuesta..."
+                    label="Escriba su respuesta..."
                     name={pregunta.label}
                     onChange={e => this.handlePreguntaChange(e)}
                 />;
@@ -134,7 +445,7 @@ class Preentrevista extends Component {
         this.parentLabel = pregunta.label;
 
         arrayPregunta.push(
-            <React.Fragment>
+            <React.Fragment key={pregunta.label}>
                 {enunciado}
                 {opciones}
             </React.Fragment>
---
+++ src/models/preentrevista-new.js
@@ -0,0 +1,91 @@
+const preguntas = [
+    {
+        id: "0",
+        label: "¿Ha realizado modificaciones al diseño de la práctica educativa apoyada en TIC que presentó?",
+        typeOfLevel: "ROOT",
+        typeOfAnswer: "RADIO",
+        options: ["Sí", "No"],
+        isTriggerFor: "1"
+    },
+    {
+        id: "1",
+        label: "¿Para qué ha modificado el diseño de su práctica educativa apoyada en TIC? (Puede elegir más de una opción)",
+        typeOfLevel: "MIDDLE",
+        typeOfAnswer: "CHECKBOX",
+        options: ["Para facilitar la presentación, almacenamiento, transmisión o intercambio de información.", "Para facilitar el acceso y la búsqueda de información de calidad.", "Para aprovechar los recursos del escenario educativo.", "Para utilizar herramientas TIC novedosas, estéticas o accesibles."],
+        isTriggeredBy: "0.0"
+    },
+    {
+        id: "2",
+        label: "Explique. (Máximo 400 caracteres)",
+        typeOfLevel: "FINAL",
+        typeOfAnswer: "INPUT",
+        descriptores: ["I1ta"],
+        evidencia: "Cambios en (1) la manera de presentar los contenidos; (2) la forma de almacenar y compartir información; (3) manera de facilitar el acceso a información de calidad.",
+        isTriggeredBy: "1.0"
+    },
+    {
+        id: "3",
+        label: "Explique. (Máximo 400 caracteres)",
+        typeOfLevel: "FINAL",
+        typeOfAnswer: "INPUT",
+        descriptores: ["I1ta"],
+        evidencia: "Cambios en (1) la manera de presentar los contenidos; (2) la forma de almacenar y compartir información; (3) manera de facilitar el acceso a información de calidad.",
+        isTriggeredBy: "1.1"
+    },
+    {
+        id: "4",
+        label: "Explique. (Máximo 400 caracteres)",
+        typeOfLevel: "FINAL",
+        typeOfAnswer: "INPUT",
+        descriptores: ["R1tb"],
+        evidencia: "Modificaciones para aprovechar al máximo los recursos TIC en el escenario educativo.",
+        isTriggeredBy: "1.2"
+    },
+    {
+        id: "5",
+        label: "Explique. (Máximo 400 caracteres)",
+        typeOfLevel: "FINAL",
+        typeOfAnswer: "INPUT",
+        descriptores: ["R1tc"],
+        evidencia: "Ejemplo de introducción de TIC siguiendo criterios de novedad, estética y accesibilidad.",
+        isTriggeredBy: "1.3"
+    },
+    {
+        id: "6",
+        label: "¿Qué tuvo en cuenta para modificar el diseño de su práctica educativa apoyada en TIC? (Puede elegir más de una opción)",
+        typeOfLevel: "MIDDLE",
+        typeOfAnswer: "CHECKBOX",
+        options: ["Experiencias previas de diseño de prácticas educativas apoyadas en TIC.", "Necesidades de los estudiantes y características de los contenidos.", "El potencial educativo de las TIC."],
+        isTriggeredBy: "0.0"
+    },
+    {
+        id: "7",
+        label: "Explique. (Máximo 400 caracteres)",
+        typeOfLevel: "FINAL",
+        typeOfAnswer: "INPUT",
+        descriptores: ["R1ta"],
+        evidencia: "Ejemplo de herramientas nuevas introducidas. Se compara la práctica actual con versiones previas.",
+        isTriggeredBy: "6.0"
+    },
+    {
+        id: "8",
+        label: "Explique. (Máximo 400 caracteres)",
+        typeOfLevel: "FINAL",
+        typeOfAnswer: "INPUT",
+        descriptores: ["E1ta"],
+        evidencia: "Ejemplo de cambio introducido por características de los contenidos o realimentaciones de los estudiantes.",
+        isTriggeredBy: "6.1"
+    },
+    {
+        id: "9",
+        label: "Explique. (Máximo 400 caracteres)",
+        typeOfLevel: "FINAL",
+        typeOfAnswer: "INPUT",
+        descriptores: ["E1tb", "E1tc"],
+        evidencia: "Muestra de modificación introducida a la práctica educativa apoyada en TIC a partir de la identificación de potencialidades de dichas herramientas.",
+        isTriggeredBy: "6.2"
+    }
+];
+
+export default preguntas;
